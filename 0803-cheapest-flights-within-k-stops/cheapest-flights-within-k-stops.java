32e93d084c7006e85bb35916784112d8b6848545
Sarang <46310322+i-supermario@users.noreply.github.com>
2025-03-14 10:37:44 -0700

    Time: 20 ms (8.11%) Space: 44.3 MB (83.49%)

---
+++ 0803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        int[][] matrix = new int[n][n];
+        for(int[] r: matrix) Arrays.fill(r,Integer.MAX_VALUE);
+
+        for(int[] f: flights){
+            matrix[f[0]][f[1]] = f[2];
+        }
+
+        int[] og = new int[n];
+        Arrays.fill(og, Integer.MAX_VALUE);
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(src);
+        og[src] = 0;
+
+        while(!q.isEmpty() && k>-1){
+            k--;
+            System.out.println("Layer");
+            System.out.println(k);
+            int size = q.size();
+            int[] dist = og.clone();
+            for(int j=0;j<size;j++){
+                int curr = q.poll();
+                for(int i=0;i<n;i++){
+
+                    if(matrix[curr][i]!=Integer.MAX_VALUE && dist[curr] + matrix[curr][i] < og[i]){
+                        System.out.println("Route update");
+                        System.out.println(curr);
+                        System.out.println(i);
+                        og[i] = dist[curr] + matrix[curr][i];
+                        System.out.println(og[i]);
+                        q.add(i);
+
+                    }
+
+                }
+            }
+
+        }
+
+        return og[dst]==Integer.MAX_VALUE ? -1 : og[dst];
+
+    }
+}
