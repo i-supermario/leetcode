0682b952479f87035ddd431c50d39bb35f6a7b4c
Sarang <46310322+i-supermario@users.noreply.github.com>
2025-04-04 19:00:17 -0700

    Time: 0 ms (100.00%) Space: 42.9 MB (36.79%)

--- 0104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -17,25 +17,10 @@ import java.lang.Math;
 import java.util.*;
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        int height = 0;
-        while(!q.isEmpty()){
-            height++;
-            int size = q.size();
-            for(int i=0;i<size;i++){
-
-                TreeNode curr = q.poll();
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-
-            }
-
-        }
-
-        return height;
-        
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right)); 
 
     }
+
+    
 }
