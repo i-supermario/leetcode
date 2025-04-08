6c772ceadc597c82a7ec2bc4f8b1510ed873d117
Sarang <46310322+i-supermario@users.noreply.github.com>
2025-04-08 10:37:28 -0700

    Time: 2 ms (80.76%) Space: 46.5 MB (8.28%)

--- 0055-jump-game/jump-game.java
+++ 0055-jump-game/jump-game.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length <= 1) return true;
-        boolean[] dp = new boolean[nums.length];
-        dp[0] = true;
+        int maxReachable = 0;
 
         for(int i=0;i<nums.length-1;i++){
 
-            if(dp[i]){
-                for(int j=i+1;j<=i+nums[i];j++){
-                    if(j <= nums.length - 1){
-                        dp[j] = true;
-                    } 
-                }
+            if(i <= maxReachable){
+                maxReachable = Math.max(maxReachable, i + nums[i]);
             }
-            if(dp[nums.length-1]) return true;
+            if(maxReachable >= nums.length - 1) return true;
 
         }
 
-        return dp[nums.length -1];
+        return false;
         
     }
 }
