61623cba34a27c88bf406b2e1f9071f64085e50c
Sarang <46310322+i-supermario@users.noreply.github.com>
2025-04-20 20:32:45 -0700

    Time: 0 ms (100.00%) Space: 49.9 MB (7.88%)

---
+++ 0344-reverse-string/reverse-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        int i = 0;
+        int j = s.length - 1;
+
+        char temp = '_';
+
+        while(i < j){
+
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+
+        }
+
+        return;
+        
+    }
+}
