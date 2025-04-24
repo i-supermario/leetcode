69cdf7d6517af3c791b368bc0e19d2f1f7a4affa
Sarang <46310322+i-supermario@users.noreply.github.com>
2025-04-23 17:22:05 -0700

    Time: 1 ms (85.33%) Space: 41.1 MB (41.49%)

--- 0069-sqrtx/sqrtx.java
+++ 0069-sqrtx/sqrtx.java
@@ -1,5 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.pow(x, 0.5);
+        if(x <= 1) return x;
+        int low = 1;
+        int high = x / 2;
+        int mid = 1;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(mid*mid == x){
+                return mid;
+            }
+            else if( (long) mid*mid < (long) x) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return Math.round(high);
+        
+        
+
     }
 }
